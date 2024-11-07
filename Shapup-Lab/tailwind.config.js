5743ad8fcc04fd21095b62dfd2df4416b6d21ac6
Ed Byrne <ed.byrne@cna.nl.ca>
2024-11-07 10:29:33 -0330

    tailwindcss and shapup

---
+++ Shapup-Lab/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './index_3_column.html',
+    './strength/*.html',
+    './stress/*.html',
+    './diet/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
